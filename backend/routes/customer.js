8e73146a81ffdfd47fc4bc719e13a8b547e99b48
freeelancer <lmengsplist96@gmail.com>
2021-03-12 14:09:19 +0800

    created smart contract with variables

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  // req.body.accountAddress
+  // req.body.deliveryAddress
+  res.send('respond with a resource');
+});
+
 module.exports = router;
