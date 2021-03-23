e32362c092fa4b134da599f7506fa87e25cac9ce
Zhu Bolin <zhubolin@C02ZM0BBLVDT.local>
2021-03-23 19:28:11 +0800

    added voting page and respective functionalities

--- deliver/src/customer/Customer.jsx
+++ deliver/src/customer/Customer.jsx
@@ -1,20 +1,32 @@
 import React, {Component} from 'react'
 import OrderForm from './OrderForm'
 import OrderList from './OrderList'
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
 
 class Customer extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            AccountNumber: this.props.location.state ? this.props.location.state.AccountNumber: '' ,
+            IsCustomer: this.props.location.state ? this.props.location.state.IsCustomer: ''
+        }
+        // console.log(this.state)
     }
 
     render() {
-        return (
-            <div className='customerApp' style={{width:'100%', display: "inline-block"}}>
-                <header style={{ background: '#4B9CD3', color: '#fff', padding: '10px 20px' }}> DEliver | Receiver </header>
-                <OrderList/>
-                <OrderForm/>
-            </div>
-        );
+        if (this.state.IsCustomer) {
+            return (
+                <div className='customerApp' style={{width:'100%', display: "inline-block"}}>
+                    <header style={{ background: '#4B9CD3', color: '#fff', padding: '10px 20px' }}> DEliver | Customer </header>
+                    <OrderList AccountNumber={this.state.AccountNumber}/>
+                    <OrderForm AccountNumber={this.state.AccountNumber}/>
+                </div>
+            );            
+        }
+        else{
+            return <Redirect to="/voting"></Redirect>      
+        }
     }
 }
 
