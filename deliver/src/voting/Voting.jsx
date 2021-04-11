72949023d3025f60b8506afb717acb8bc27ffa06
rachelyt98 <61983696+rachelyt98@users.noreply.github.com>
2021-04-11 16:28:32 +0800

    Edited Voting

--- deliver/src/voting/Voting.jsx
+++ deliver/src/voting/Voting.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
 import './VotingForm.css'
+import axios from 'axios';
 
 
 class Voting extends Component {
@@ -9,11 +10,12 @@ class Voting extends Component {
         this.state={
             AccountNumber: '',
             CustomerOrRider: '',
-            Content: 'dasjlauindlausnvufabvuilbfaluivbluadb uhuibnaiubub  dasudbausbdubaubdu auduasdhaushduasudhaus asuhduahsudhuahdas auhduhuhasudhuashdu ahusduahsuhdus',
+            Content: [],
             Supporting: 'Rider'
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmitAccountNumber = this.handleSubmitAccountNumber.bind(this);
+        this.handleVote = this.handleVote.bind(this);
     }
     
     handleChange(event){
@@ -32,6 +34,22 @@ class Voting extends Component {
         }
     }
 
+    componentDidMount() {
+        const data = { user: this.state.AccountNumber };
+        fetch('http://localhost:5000/customer/isCustomer',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then(list => {
+            this.setState({ CustomerOrRider: "Customer" });
+        });
+    }
+
     handleSubmitAccountNumber(event){
         // need to retrieve information on whether this account is a customer or rider
         if (this.state.AccountNumber === ''){
@@ -39,11 +57,55 @@ class Voting extends Component {
         } 
         // the entered account number could be wrong     
         else{
-            this.setState({
-                // dummy, set to customer mannually
-                CustomerOrRider: "Customer"
+            const data = { user: this.state.AccountNumber };
+            //alert(this.state.AccountNumber)
+            /*fetch('http://localhost:5000/customer/isCustomer',{
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
             })
+            .then((response) => response.json())
+            .then(list => {
+                this.setState({ CustomerOrRider: "Customer"});
+            })
+            .catch(error => {console.log(error)});
+            alert(this.state.CustomerOrRider)*/
+
+            //this.setState({CustomerOrRider: "Customer"});
+
+            fetch('http://localhost:5000/voting/getConflict',{
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+            .then((response) => response.json())
+            .then(list => {
+                this.setState({ Content: list });
+            });
         }  
+    }
+
+    handleVote(id){
+        const data = { 
+            user: this.state.AccountNumber,
+            vote: this.state.Supporting === 'Customer' ? true : false,
+            orderId: id
+        };
+        fetch('http://localhost:5000/voting/vote',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then(res => res.json())
+        .catch(error => {console.log(error)})
     }
 
     // 1. get a random conflict 
@@ -70,7 +132,15 @@ class Voting extends Component {
                 <div className='votingApp'>
                     <header style={{ background: '#4B9CD3', color: '#fff', padding: '10px 20px' }}> DEliver | Voting </header>
                     <form className='votingForm'>
-                        <p style={{textAlign:'center', fontSize:'20px',font:'serif', whiteSpace:'pre-line', marginTop:"100px", marginLeft:"20px", marginRight:"20px"}}>{this.state.Content}</p>
+                        {
+                            <li style={{textAlign:'center', fontSize:'20px',font:'serif', whiteSpace:'pre-line', marginTop:"100px", marginLeft:"20px", marginRight:"20px"}}>
+                                <p>Restaurant: {this.state.Content["restaurant"]}</p>
+                                <p>Food Fee: {this.state.Content["foodFee"]}</p>
+                                <p>Delivery Fee: {this.state.Content["deliveryFee"]}</p>
+                                <p>Customer Complaint: {this.state.Content["customerComplaint"]}</p>
+                                <p>Rider Complaint: {this.state.Content["riderComplaint"]}</p>
+                            </li>
+                        }
                         <label className='form-label' style={{marginLeft:"33%" ,display:"block", top:"60%", textAlign:'center', alignContent:'center', position:"absolute", display:"block", fontWeight:"bold"}}>Who do you support?</label>
                         <select name='Supporting' style={{textAlign:'center', top: "70%", borderRadius: "2px", height: "36px", width: "300px", display:"inline-block", marginLeft:"27%", position:"absolute"}} onChange={this.handleChange}>
                             <option>Rider</option>
@@ -82,7 +152,7 @@ class Voting extends Component {
                                 AccountNumber: this.state.AccountNumber,
                                 IsCustomer: this.state.CustomerOrRider === 'Customer' ? true : false
                             }
-                        }} className='form-input-btn' type='submit' style={{ height: '30px' ,textAlign:'center', textDecoration:"none", marginLeft:"14%", paddingTop:"2.5%", top:"80%", position:"absolute", display:"block", alignContent:'center'}}>
+                        }} className='form-input-btn' type='submit' onClick={this.handleVote(this.state.Content["orderId"])} style={{ height: '30px' ,textAlign:'center', textDecoration:"none", marginLeft:"14%", paddingTop:"2.5%", top:"80%", position:"absolute", display:"block", alignContent:'center'}}>
                             Vote
                         </Link>
                     </form>
